5114f7c6329b502543f75d290b607d166732b08c
Denis <vin_ogr@list.ru>
2024-06-03 22:16:05 +0700

    Initial commit

---
+++ object_metods/main.py
@@ -0,0 +1,15 @@
+class House:
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+    def go_to(self, floor):
+        if floor < 1 or floor > self.number_of_floors:
+            print("Такого этажа не существует")
+        else:
+            for i in range(floor):
+                print(i+1)
+
+h1 = House('ЖК Горский', 18)
+h2 = House('Домик в деревне', 2)
+h1.go_to(5)
+h2.go_to(10)
