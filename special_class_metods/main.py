362bbdd3bab42f44b0bf10e0996d7835b80b7609
Denis <vin_ogr@list.ru>
2024-06-03 23:05:46 +0700

    Специальные методы классов

---
+++ special_class_metods/main.py
@@ -0,0 +1,11 @@
+class House:
+    def __init__(self):
+        self.numberOfFloors = 0
+
+    def setNewNumberOfFloors(floors):
+        numberOfFloors = floors
+        print(numberOfFloors)
+
+h1 = House
+h1.setNewNumberOfFloors(3)
+
