ebfa56b0bdcb244e1804be7b9f53215cad2329fe
Denis <vin_ogr@list.ru>
2024-10-27 22:58:28 +0700

    reload method

--- reload_method/module_5_3.py
+++ reload_method/module_5_3.py
@@ -1,4 +1,39 @@
 class House:
+    def __str__(self):
+        return f'Название: {self.name}, кол-во этажей: {self.number_of_floors}'
+    def __eq__(self, other):
+        return self.number_of_floors == other
+    def __lt__(self, other):
+        return self.number_of_floors < other
+    def __le__(self, other):
+        return self.number_of_floors <= other
+    def __gt__(self, other):
+        return self.number_of_floors > other
+    def __ge__(self, other):
+        return self.number_of_floors >= other
+    def __ne__(self, other):
+        return self.number_of_floors != other
+    def __add__(self, value):
+        if isinstance(self, House):
+            if not isinstance(value, (int, House)):
+                raise ArithmeticError("Правый операнд должен быть типом int или объектом House")
+            floor = value if isinstance(value, int) else value.number_of_floors
+            self.number_of_floors += floor
+            return (self)
+    def __radd__(self, value):
+        if isinstance(self, House):
+            if not isinstance(value, (int, House)):
+                raise ArithmeticError("Правый операнд должен быть типом int или объектом House")
+            floor = value if isinstance(value, int) else value.number_of_floors
+            self.number_of_floors += floor
+            return (self)
+    def __iadd__(self, value):
+        if isinstance(self, House):
+            if not isinstance(value, (int, House)):
+                raise ArithmeticError("Правый операнд должен быть типом int или объектом House")
+            floor = value if isinstance(value, int) else value.number_of_floors
+            self.number_of_floors += floor
+            return (self)
     def __init__(self, name, number_of_floors):
         self.name = name
         self.number_of_floors = number_of_floors
@@ -9,7 +44,26 @@ class House:
             for i in range(floor):
                 print(i+1)
 
-h1 = House('ЖК Горский', 18)
-h2 = House('Домик в деревне', 2)
-h1.go_to(5)
-h2.go_to(10)
+h1 = House('ЖК Эльбрус', 10)
+h2 = House('ЖК Акация', 20)
+
+print(h1)
+print(h2)
+
+print(h1 == h2) # __eq__
+
+h1 = h1 + 10 # __add__
+print(h1)
+print(h1 == h2)
+
+h1 += 10 # __iadd__
+print(h1)
+
+h2 = 10 + h2 # __radd__
+print(h2)
+
+print(h1 > h2) # __gt__
+print(h1 >= h2) # __ge__
+print(h1 < h2) # __lt__
+print(h1 <= h2) # __le__
+print(h1 != h2) # __ne__
