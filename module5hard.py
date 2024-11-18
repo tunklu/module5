382400c5405a1239aa176c41f584689e5d31bc8d
Denis <vin_ogr@list.ru>
2024-11-18 13:17:22 +0700

    Дополнительное задание: "Классы и объекты."

--- module5hard.py
+++ module5hard.py
@@ -87,7 +87,7 @@ class UrTube:
                 video.time_now = 0
                 return
 
-
+# Код для проверки:
 ur = UrTube()
 v1 = Video('Лучший язык программирования 2024 года', 200)
 v2 = Video('Для чего девушкам парень программист?', 10, adult_mode=True)
